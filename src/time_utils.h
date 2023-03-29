6d8e5b578e4c0e0c1159068ed5822ee1d6d3d898
Azuelle <tjk@live.cn>
2023-03-29 22:10:55 +0800

    Move all time-related consts to time_utils

--- src/time_utils.h
+++ src/time_utils.h
@@ -3,4 +3,12 @@
 #ifndef TIME_UTILS_H
 #define TIME_UTILS_H
 
+#include <chrono>
+
+using namespace std::chrono;
+
+static const int FRAMES_PER_SECOND = 30;
+static microseconds const FRAME_TIME =
+    seconds(1) / FRAMES_PER_SECOND;  // Length of a frame
+
 #endif
