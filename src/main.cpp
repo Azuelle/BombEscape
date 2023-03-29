5d3f0fa28475b595fc6e54ec7d696adae8b9a465
Azuelle <tjk@live.cn>
2023-03-29 22:10:55 +0800

    Move all time-related consts to time_utils

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,9 @@
 #include "display.h"
 #include "game_logic.h"
 #include "gamestate.h"
-
-const int FRAMES_PER_SECOND = 30;
+#include "time_utils.h"
 
 int main() {
-    using namespace std::chrono;
-    microseconds frame_time =
-        seconds(1) / FRAMES_PER_SECOND;  // Length of a frame
-
     Input usr_input = Input::none;
     GameState gamestate;
 
@@ -33,7 +28,7 @@ int main() {
         usr_input = getInput();
         logic(gamestate, usr_input);
 
-        usleep(frame_time.count());  // Wait until next frame
+        usleep(FRAME_TIME.count());  // Wait until next frame
     }
 
     // Cleanup for curses
