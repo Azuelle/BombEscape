8a2fec6e749aa59e53b22903aaddd2198fa745dc
Azuelle <tjk@live.cn>
2023-03-31 23:51:33 +0800

    Finish implementation of print() & mvprint()

--- src/io_utils.cpp
+++ src/io_utils.cpp
@@ -24,18 +24,38 @@ Input getInput() {
     return usr_input;
 }
 
-int print(std::string content, int max_len = -1) {
-    return addnstr(content.c_str(), max_len);
-}
 template <typename T>
 int print(T content, int max_len = -1) {
     return addnstr(std::to_string(content).c_str(), max_len);
 }
-
-int mvprint(std::string content, int x, int y, int max_len = -1) {
-    return mvaddnstr(y, x, content.c_str(), max_len);
+template <>
+int print(std::string content, int max_len) {
+    return addnstr(content.c_str(), max_len);
 }
+template <>
+int print(char content, int max_len) {
+    char str[2] = {content};
+    return addnstr(str, max_len);
+}
+template <>
+int print(bool content, int max_len) {
+    return addnstr(content ? "true" : "false", max_len);
+}
+
 template <typename T>
 int mvprint(T content, int x, int y, int max_len = -1) {
     return mvaddnstr(y, x, std::to_string(content).c_str(), max_len);
+}
+template <>
+int mvprint(std::string content, int x, int y, int max_len) {
+    return mvaddnstr(y, x, content.c_str(), max_len);
+}
+template <>
+int mvprint(char content, int x, int y, int max_len) {
+    char str[2] = {content};
+    return mvaddnstr(y, x, str, max_len);
+}
+template <>
+int mvprint(bool content, int x, int y, int max_len) {
+    return mvaddnstr(y, x, content ? "true" : "false", max_len);
 }
