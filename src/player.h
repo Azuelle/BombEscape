19f1f18d9a3804fb0aa2d3f41cc3f42ee8153416
Zhao-Shuman <125121062+Zhao-Shuman@users.noreply.github.com>
2023-04-04 20:29:07 +0800

    Update player.h

--- src/player.h
+++ src/player.h
@@ -4,34 +4,30 @@
 #define PLAYER_H_
 
 // Describes player actions
-enum PState
-{
-    PState_walk,
-    PState_walk_hold,
-    PState_death,
-    PState_place, //Placing a bomb
-    PState_frozen //Frozen
+enum PState {
+  PState_walk,
+  PState_walk_hold,
+  PState_death,
+  PState_place, //Placing a bomb
+  PState_frozen //Frozen
 };
 
 // Describes player health state
-enum PHealth
-{
-    PHealth_alive, //Normal state
-    PHealth_dying, //Low HP
-    PHealth_dead //HP <= 0
+enum PHealth {
+  PHealth_alive, //Normal state
+  PHealth_dying, //Low HP
+  PHealth_dead //HP <= 0
 };
 
 // Describes shield state
-enum PShield
-{
-    PShield_none,
-    PShield_normal,
-    PShield_vanishing
+enum PShield {
+  PShield_none,
+  PShield_normal,
+  PShield_vanishing
 };
 
-class Player
-{
-private:
+class Player {
+  private:
     PState p_state;
     PHealth p_health;
     PShield p_shield;
@@ -45,44 +41,44 @@ private:
     double p_shield_time;
     double p_speed_time;
     double p_frozen_time;
-public:
+  public:
     Player();
     ~Player();
-    inline bool IsAlive();
-    inline bool IsDying();
-    inline bool IsDead();
-    inline bool HasShield();
-    inline int PositionX();
-    inline int PositionY();
-    inline int GetBombs();
-    inline int GetBombMax();
-    inline int GetBombMaxToPlace();
-    inline int GetHealth();
-    inline double GetShieldTime();
-    inline double GetSpeedTime();
-    inline double GetFrozenTime();
+    inline bool isAlive();
+    inline bool isDying();
+    inline bool isDead();
+    inline bool hasShield();
+    inline int positionX();
+    inline int positionY();
+    inline int getBombs();
+    inline int getBombMax();
+    inline int getBombMaxToPlace();
+    inline int getHealth();
+    inline double getShieldTime();
+    inline double getSpeedTime();
+    inline double getFrozenTime();
 
-    inline PState GetState();
-    inline PHealth GetHealthState();
-    inline PShield GetShield();
+    inline PState getState();
+    inline PHealth getHealthState();
+    inline PShield getShield();
 };
 
-inline bool Player::IsAlive() { return p_health == PHealth_alive; }
-inline bool Player::IsDying() { return p_health == PHealth_dying; }
-inline bool Player::IsDead() { return p_health == PHealth_dead; }
-inline bool Player::HasShield() { return p_shield == PShield_normal || p_shield == PShield_vanishing; }
-inline int Player::GetBombs() { return p_bomb_num; }
-inline int Player::GetBombMax() { return p_bomb_max; }
-inline int Player::GetBombMaxToPlace() { return p_bomb_max_to_place; }
-inline int Player::GetHealth() { return p_health_val; };
-inline int Player::PositionX() { return p_x; }
-inline int Player::PositionY() { return p_y; }
-inline PHealth Player::GetHealthState() { return p_health; }
-inline PShield Player::GetShield() { return p_shield; }
-inline PState Player::GetState() { return p_state; }
-inline double Player::GetShieldTime() { return p_shield_time; }
-inline double Player::GetSpeedTime() { return p_speed_time; }
-inline double Player::GetFrozenTime(){ return p_frozen_time; }
+inline bool Player::isAlive() { return p_health == PHealth_alive; }
+inline bool Player::isDying() { return p_health == PHealth_dying; }
+inline bool Player::isDead() { return p_health == PHealth_dead; }
+inline bool Player::hasShield() { return p_shield == PShield_normal || p_shield == PShield_vanishing; }
+inline int Player::getBombs() { return p_bomb_num; }
+inline int Player::getBombMax() { return p_bomb_max; }
+inline int Player::getBombMaxToPlace() { return p_bomb_max_to_place; }
+inline int Player::getHealth() { return p_health_val; };
+inline int Player::positionX() { return p_x; }
+inline int Player::positionY() { return p_y; }
+inline PHealth Player::getHealthState() { return p_health; }
+inline PShield Player::getShield() { return p_shield; }
+inline PState Player::getState() { return p_state; }
+inline double Player::getShieldTime() { return p_shield_time; }
+inline double Player::getSpeedTime() { return p_speed_time; }
+inline double Player::getFrozenTime(){ return p_frozen_time; }
 
 
 #endif
