9f24f7ddcfeff6b84e31944d67c23315163aa98f
Jonathan <77164328+qinchonghanzuibang@users.noreply.github.com>
2023-04-20 20:50:33 +0800

    Add menu.h

---
+++ src/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <stdio.h>
+#include <ncurses.h>
+#include <curses.h>
+#include <string>
+#include <stdlib.h>
+
+int main(int argc, char **argv);
+
+#endif
