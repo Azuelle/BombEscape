f80a48097063a9be30b508c6e71bfe212430f6d0
Summerflyfly <128541843+Summerflyfly@users.noreply.github.com>
2023-05-05 23:40:19 +0800

    Add files via upload

---
+++ display.h
@@ -0,0 +1,8 @@
+#ifndef MESSAGE_DISPLAY_H
+#define MESSAGE_DISPLAY_H
+void in_put(string a);
+// use this function when input sentence
+void newest_message();
+// use this function display the newest message 
+void initialize();
+#endif
