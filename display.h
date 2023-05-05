d4c66d3ac94e0ab8dee6a40e73039bf2f892c5a1
Summerflyfly <128541843+Summerflyfly@users.noreply.github.com>
2023-05-05 23:46:39 +0800

    Delete display.h

--- display.h
+++
@@ -1,8 +0,0 @@
-#ifndef MESSAGE_DISPLAY_H
-#define MESSAGE_DISPLAY_H
-void in_put(string a);
-// use this function when input sentence
-void newest_message();
-// use this function display the newest message 
-void initialize();
-#endif
