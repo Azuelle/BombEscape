e51cb2e4a4766b4da3cc139254b89b766d43923f
Azuelle <tjk@live.cn>
2023-05-05 18:03:51 +0800

    Fix `entity` and add entities in map_test

--- src/render.cpp
+++ src/render.cpp
@@ -2,6 +2,8 @@
 
 #include "render.h"
 
+#include <iostream>
+
 // return a vector<string> containing the icons of the map
 
 // wall # player @ bomb O barricade ! floor .
@@ -14,13 +16,13 @@ std::vector<std::string> renderMap(GameState &state) {
             if (state.playfield->isObstacle(j, i)) vec[i].at(j) = '#';
 
     for (auto itr = state.playfield->entity_list.begin();
-         itr != state.playfield->entity_list.end(); itr++)
+         itr != state.playfield->entity_list.end(); itr++) {
         vec[(*itr)->getPosition().y][(*itr)->getPosition().x] =
             (*itr)->getIcon();
+    }
 
-    for (int i = 0; i < state.playfield->getHeight(); i++)
-        for (int j = 0; j < state.playfield->getWidth(); j++)
-            if (state.player->getPosition() == Pos{j, i}) vec[i][j] = '@';
+    Pos p_pos = state.player->getPosition();
+    vec[p_pos.y][p_pos.x] = state.player->getpIcon();
 
     return vec;
 }
--- test/map_test.cpp
+++ test/map_test.cpp
@@ -10,6 +10,10 @@ int main() {
     Playfield map(30, 14);
     Player pl;
     pl.setPosition({1, 1});
+    map.entity_list.push_back(new Bomb(Pos{6, 6}, 3));
+    map.entity_list.push_back(new Barricade(Pos{6, 7}));
+    map.entity_list.push_back(new Healing(Pos{6, 8}));
+    map.entity_list.push_back(new BombPower(Pos{6, 9}));
     GameState gs(&pl, &map);
     vector<string> m = renderMap(gs);
     for (auto i : m) cout << i << endl;
